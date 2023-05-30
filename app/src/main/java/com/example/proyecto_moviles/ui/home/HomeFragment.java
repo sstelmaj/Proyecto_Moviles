c8b5bf131e841cb9dc8c4050682ecb43aec64dd9
paulo-ruiz <paulo.ruizanderson@gmail.com>
2023-05-30 18:14:06 -0300

    Cambios iniciales

--- app/src/main/java/com/example/proyecto_moviles/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/proyecto_moviles/ui/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.proyecto_moviles.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,21 +11,35 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyecto_moviles.Modelo.Libro;
 
+import com.example.proyecto_moviles.R;
+import com.example.proyecto_moviles.adapter.librosAdapter;
 import com.example.proyecto_moviles.databinding.FragmentHomeBinding;
+import com.example.proyecto_moviles.rest.librosApiAdapter;
+import com.example.proyecto_moviles.utils.OnItemClickListener;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    private TextView titulo;
+    //private Retrofit retrofit;
+    private librosAdapter adapter;
 
+    private RecyclerView recyclerView;
     private Libro libroPrueba1 = new Libro();
-    private Libro libroPrueba2= new Libro();
+    private Libro libroPrueba2 = new Libro();
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -34,10 +50,11 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        recyclerView = binding.recyclerView;
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
 
-        libroPrueba1.setId(1);
+        /*libroPrueba1.setId(1);
         libroPrueba1.setIsbn("978-8490353787");
         libroPrueba1.setTitulo("Introducción a la programación con Python");
         libroPrueba1.setDescripcion("Este libro es una introducción a la programación en Python, un lenguaje de programación muy popular y poderoso que se utiliza en una gran variedad de campos, desde la informática hasta la biología.");
@@ -55,6 +72,30 @@ public class HomeFragment extends Fragment {
         libroPrueba1.setVigente('S');
         libroPrueba1.setPuntuacion(4.7);
 
+        final TextView titulo = binding.textView2;
+        titulo.setText(libroPrueba1.getTitulo());*/
+        connectAndGetApiData();
+
+
+        /*List<Libro> libros = new ArrayList<>();
+        libros.add(libroPrueba1);
+        libros.add(libroPrueba1);*/
+
+        //enlazo el clicklistener al adapter
+        /*adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(Libro item) {
+                //Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+                Intent i = new Intent(MainActivity.this, MovieDetail.class);
+                i.putExtra("pelicula", item);
+
+                startActivity(i);
+                //finish(); //Si quiero cerrar la actividad y quitarla del stack
+
+            }
+        });
+        recyclerView.setAdapter(adapter);*/
+
         return root;
     }
 
@@ -62,5 +103,22 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void connectAndGetApiData() {
+        Call<List<Libro>> call = librosApiAdapter.getApiService().getLibros();
+        call.enqueue(new Callback<List<Libro>>(){
+            @Override
+            public void onResponse(Call<List<Libro>> call, Response<List<Libro>> response) {
+                if (response.isSuccessful()) {
+                    List<Libro> libros=response.body();
+                    adapter = new librosAdapter(libros, R.layout.list_item_libro, getActivity());
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Libro>> call, Throwable t) {
+            }
+        });
     }
 }
